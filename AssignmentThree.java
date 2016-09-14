a7c4fc66ad895379e747d26cad1605ebcea65ddf
Aman Turate <amanturate@users.noreply.github.com>
2016-09-14 09:43:10 +0530

    Add files via upload

---
+++ AssignmentThree.java
@@ -0,0 +1,29 @@
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class AssignmentThree {
+	
+	public static void main(String[] args) throws IOException {
+
+		String filePath = new File("Input.txt").getAbsolutePath();
+		String filePath1 = new File("Your.txt").getAbsolutePath();
+		
+		FileInputStream fis = new FileInputStream(filePath);
+		FileOutputStream fos = new FileOutputStream(filePath1);
+		
+		int data = fis.read();
+		
+		while(data != -1){
+			fos.write(data);
+			data = fis.read();
+		}
+		
+		fis.close();
+		fos.close();
+	}
+
+}
